e4436f3ee5947e1214e0907051a0cdefe7c81a67
Prabir Tarafdar <ptarafdar010@gmail.com>
2021-06-07 17:04:49 +0530

    first comit

---
+++ prime_factor.cpp
@@ -0,0 +1,39 @@
+// prime factorisation using sieve
+
+#include <iostream>
+using namespace std;
+
+void primefactor(int n)
+{
+
+    int spf[100] = {0};
+    for (int i = 2; i <= n; i++)
+    {
+        spf[i] = i;
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (spf[i] == i)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                spf[j] = i;
+            }
+        }
+    }
+    while (n != 1)
+    {
+        cout << spf[n] << " ";
+        n = n / spf[n];
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    primefactor(n);
+
+    return 0;
+}
